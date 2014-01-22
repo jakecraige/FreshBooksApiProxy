6051eefbf13889c7dcf14b8c565deff8aee3c2d4
Jake Craige <jake@poeticsystems.com>
2014-01-21 21:28:00 -0600

    initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.2'
+gem 'pg'
+
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+
+gem 'ruby-freshbooks'
+gem 'airbrake'
+
+group :development, :test do
+  gem 'debugger'
+end
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'meta_request'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FreshbooksApi::Application.config.secret_key_base = '89eea76f7ed09a1298fccd78698d930352b048ee922374224092f7de0f902aa3c41b2a5fc01c07db8cbc2bb7ea94c569741e1c80b4e9808a95925555aaf5dc4d'
